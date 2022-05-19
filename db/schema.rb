a395d5238bf510bf4c06741ea132db38b692272d
Gokhan T <trgtgokhan@gmail.com>
2022-05-19 23:55:47 +0300

    Remove example scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_203141) do
-  create_table "cars", force: :cascade do |t|
-    t.string "make"
-    t.string "model"
-    t.integer "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
